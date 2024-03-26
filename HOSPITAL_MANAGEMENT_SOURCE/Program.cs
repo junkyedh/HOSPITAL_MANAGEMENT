878b36a4426b94003cb6c174b322312d784b7ec0
ToHoanghuy <22520573@gm.uit.edu.vn>
2024-03-26 15:24:33 +0700

    updated Form Assign and Bill Detail

--- HOSPITAL_MANAGEMENT_SOURCE/Program.cs
+++ HOSPITAL_MANAGEMENT_SOURCE/Program.cs
@@ -16,7 +16,7 @@ namespace HOSPITAL_MANAGEMENT_SOURCE
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GUI.FormBillDetail());
         }
     }
 }
